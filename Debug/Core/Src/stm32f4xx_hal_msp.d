98fad939e7d1df8490b91bd877c8cb9eaa7b53fb
Adi <adityarugge2@gmail.com>
2025-06-23 01:54:04 +0200

    Step 7 : Segger integration

--- Debug/Core/Src/stm32f4xx_hal_msp.d
+++ Debug/Core/Src/stm32f4xx_hal_msp.d
@@ -25,7 +25,21 @@ Core/Src/stm32f4xx_hal_msp.o: ../Core/Src/stm32f4xx_hal_msp.c \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
- ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h
+ ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/FreeRTOSConfig.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/OS/SEGGER_SYSVIEW_FREERTOS.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/SEGGER/SEGGER_SYSVIEW.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/SEGGER/SEGGER.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/Config/Global.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/Config/SEGGER_SYSVIEW_Conf.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/Config/SEGGER_RTT_Conf.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/include/projdefs.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/include/portable.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/include/mpu_wrappers.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -54,3 +68,17 @@ Core/Src/stm32f4xx_hal_msp.o: ../Core/Src/stm32f4xx_hal_msp.c \
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/include/FreeRTOS.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/FreeRTOSConfig.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/OS/SEGGER_SYSVIEW_FREERTOS.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/SEGGER/SEGGER_SYSVIEW.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/SEGGER/SEGGER.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/Config/Global.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/SEGGER/SEGGER_SYSVIEW_ConfDefaults.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/Config/SEGGER_SYSVIEW_Conf.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/SEGGER/Config/SEGGER_RTT_Conf.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/include/projdefs.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/include/portable.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/include/deprecated_definitions.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/Aditya/Desktop/RTOS/RTOS-workspace/001_Tasks/Third_party/FreeRTOS/include/mpu_wrappers.h:
